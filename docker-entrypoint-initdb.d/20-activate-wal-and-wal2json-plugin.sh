49ba8263e28ade4652eac038aeffcead301b6565
Luis Angel Mojena Román (lmojenaroman) <luis.mojena-roman@alayacare.com>
2020-09-22 11:11:24 -0400

    TH-515: Configure wal and wal2json plugin

---
+++ docker-entrypoint-initdb.d/20-activate-wal-and-wal2json-plugin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# install wal2json postgresql plugin
+apt update
+apt install postgresql-9.6-wal2json
+
+# activate wal
+cd "${PGDATA}"
+sed -ri "s/^#?(wal_level\s*=\s*)\S+/\1logical/" postgresql.conf
+sed -ri "s/^#?(max_wal_senders\s*=\s*)\S+/\110/" postgresql.conf
+sed -ri "s/^#?(wal_keep_segments\s*=\s*)\S+/\110/" postgresql.conf
+
+# any user with replication capabilities, from any ip can connect and replicate data
+echo "host      replication     all     0.0.0.0/0       trust" >> pg_hba.conf
